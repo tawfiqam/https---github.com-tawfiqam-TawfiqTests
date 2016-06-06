b490d9c54619de7d7128e68110645b4044f6e2e1
tawfiqam <tawfiqam@umich.edu>
2016-06-06 12:00:00 -0700

    Sixth commit - New Timer

--- App10/App10/MainPage.xaml.cs
+++ App10/App10/MainPage.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -16,7 +18,7 @@ using Windows.UI.Xaml.Navigation;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace App10
-{
+{   
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
@@ -25,13 +27,19 @@ namespace App10
         public MainPage()
         {
             this.InitializeComponent();
+
         }
+
 
         DispatcherTimer dispatcherTimer;
 
         int timesTicked = 1;
 
-        int timesToTick = 50;
+        int timesToTick = 3600;                       //This is the limit we will set for the use of the app, in this case, 1 hour
+
+        int minutesToGo = 3600;                        //60 minutes
+
+        int minuteDiff = 60;
 
         public void DispatcherTimerSetup()
         {
@@ -47,19 +55,21 @@ namespace App10
             TimerStatus.Text = "dispatcherTimer.IsEnabled = " + dispatcherTimer.IsEnabled + "\n";
 
         }
-
-
-
         void dispatcherTimer_Tick(object sender, object e)
         {
 
             TimerLog.Text = timesTicked.ToString();
+            minuteDiff = (minutesToGo - timesTicked) / 60;
+            TimerLeft.Text = minuteDiff.ToString();
+
+            //TimerLeft.Text = 
 
             if (timesTicked > timesToTick)
             {
 
                 TimerStatus.Text = "Calling dispatcherTimer.Stop()\n";
-
+                //sb.Append("StopTimer called");
+                
                 dispatcherTimer.Stop();
 
                 TimerStatus.Text = "dispatcherTimer.IsEnabled = " + dispatcherTimer.IsEnabled + "\n";
@@ -70,6 +80,7 @@ namespace App10
 
         }
 
+       
         private void TimerStart_Click_1(object sender, RoutedEventArgs e)
         {
 
@@ -95,6 +106,17 @@ namespace App10
         private void Page_Loaded_1(object sender, RoutedEventArgs e)
         {
             TimerStatus.Text = "dispatcherTimer.IsEnabled = False";
+        }
+
+        private void TimerLeft_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+           
+
+        }
+
+        private void TimerLog_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
