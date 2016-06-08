b26e19d5a8afd143ab04328abb015b8b6fe34b8f
tawfiqam <tawfiqam@umich.edu>
2016-06-07 19:07:38 -0700

    Seventh commit - UWP V2.0

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -32,20 +32,31 @@ namespace App1
         public static class Globals
         {
             public static DateTime StartTime { get; set; }
+            public static DateTime EndTime { get; set; }
             public static int pauseCount { get; set; }
             public static int morecount { get; set; }
             public static int acc15 { get; set; }
             public static int acc30 { get; set; }
+            public static DateTime Today { get; }
+            public static DispatcherTimer ds { get; set; }
 
+        }
+
+        private void SetUpTimer()
+        {
+            Globals.ds.Interval = new TimeSpan(0, 1, 0);
+            Globals.ds.Tick += ds_Tick;
         }
 
         private async void ladybug_Click(object sender, RoutedEventArgs e)
         {
+
             StorageFolder Folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
             Folder = await Folder.GetFolderAsync("Assets");
             StorageFile mediafile = await Folder.GetFileAsync("ladybug.wmv");
             media.SetSource(await mediafile.OpenAsync(FileAccessMode.Read), mediafile.ContentType);
             media.Play();
+            WriteLog("Start ladybug "); 
         }
 
         private async void fishes_Click(object sender, RoutedEventArgs e)
@@ -55,16 +66,29 @@ namespace App1
             StorageFile mediafile = await Folder.GetFileAsync("fishes.wmv");
             media.SetSource(await mediafile.OpenAsync(FileAccessMode.Read), mediafile.ContentType);
             media.Play();
+            WriteLog("Start fishes " + Globals.Today.ToString());
         }
 
         private void btnplay_Click(object sender, RoutedEventArgs e)
         {
             media.Play();
+            WriteLog("Play video " + Globals.Today.ToString());
         }
 
         private void btnpause_Click(object sender, RoutedEventArgs e)
         {
             media.Pause();
+            WriteLog("Pause " + Globals.Today.ToString());
         }
+
+        private async void WriteLog(string Msg)
+        {
+            // Create sample file; create new.
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile logfile = await storageFolder.CreateFileAsync("Log.txt", Windows.Storage.CreationCollisionOption.GenerateUniqueName);
+            await Windows.Storage.FileIO.WriteTextAsync(logfile, Msg);
+        }
+
+        
     }
 }
